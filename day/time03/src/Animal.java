daad587f34d11dee268d6a3f16ab950f41704ab1
mym-sf <453974265@qq.com>
2021-03-13 16:53:10 +0800

    第二次提交

---
+++ day/time03/src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    //成员变量
+    int age;
+    int weith;
+    int height;
+    String name;
+    Animal(){
+
+    }
+    //成员方法
+    void eat(){
+        System.out.println(this.name + "在吃东西");
+    }
+}
